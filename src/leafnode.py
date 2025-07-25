bd3d2a48993f7a7bdffcf6e2d2127e32d3f2733c
Juan Zamudio <jzamudio14@gmail.com>
2025-07-25 00:24:43 -0400

    Fix image rendering bug in LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -9,15 +9,18 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
+        if self.tag is None:
+            if self.value is None:
+                raise ValueError("all leaf nodes must have a value")
+            return self.value
+
+        if self.tag == "img":
+            return f"<img{self.props_to_html()}>"
+
         if not self.value:
             raise ValueError("all leaf nodes must have a value")
-        
-        if self.tag is None:
-            return self.value
-        
-        return (
-            f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        )
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
